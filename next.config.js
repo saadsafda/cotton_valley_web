93a566524ec8efb387df3f83c7b27259c6000b49
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-09-18 08:46:24 -0700

    update

---
+++ next.config.js
@@ -0,0 +1,66 @@
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
+
+module.exports = (phase) => {
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER
+    // when `next build` or `npm run build` is used
+    const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+    // when `next build` or `npm run build` is used
+    const isStaging =
+        phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+
+    const env = {
+        API_PROD_URL: (() => {
+            if (isDev) return 'https://cottonvalley.vercel.app/api/'
+            if (isProd) {
+                // Note: The code below needs to be uncommented, and you should use your domin where your API is hosted.
+                return 'https://cottonvalley.vercel.app/api/'
+            }
+            if (isStaging) return 'https://cottonvalley.vercel.app/api/'
+            return 'RESTURL_SPEAKERS:not (isDev,isProd && !isStaging,isProd && isStaging)'
+        })(),
+        API_BASE_URL: "https://cottonvalley.vercel.app/api",
+    }
+    const redirects = () => {
+        return [
+            {
+                source: "/",
+                destination: "/en/theme/paris",
+                permanent: true,
+            },
+        ];
+    }
+    const images = {
+        domains: ["react.pixelstrap.net", "156.67.27.94", "www.cottonvalley.net"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "react.pixelstrap.net",
+            },
+            {
+                protocol: "http",
+                hostname: "156.67.27.94", // since your link starts with http
+            },
+        ],
+    }
+
+    // const images = {
+    //     domains: ["fastkart.webiots.co.in", 'laravel.pixelstrap.net', "react.pixelstrap.net"],
+    //     remotePatterns: [
+    //         {
+    //             protocol: "https",
+    //             hostname: "fastkart.webiots.co.in",
+    //         },
+    //         {
+    //             protocol: "https",
+    //             hostname: "react.pixelstrap.net",
+    //         },
+    //     ],
+    // }
+
+    return {
+        env, redirects, images
+    }
+}
