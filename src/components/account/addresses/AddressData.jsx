35e89c2647c68790ae482118fb2d1f1c518db798
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-11-03 21:43:46 +0500

    update CVL

--- src/components/account/addresses/AddressData.jsx
+++ src/components/account/addresses/AddressData.jsx
@@ -1,55 +1,92 @@
-import ConfirmDeleteModal from '@/components/common/ConfirmDeleteModal';
-import Btn from '@/elements/buttons/Btn';
-import { useState } from 'react';
-import { RiDeleteBinLine, RiEditBoxLine } from 'react-icons/ri';
-import { Col, Row } from 'reactstrap';
-import AddressTable from './AddressTable';
-import request from '@/utils/axiosUtils';
+import ConfirmDeleteModal from "@/components/common/ConfirmDeleteModal";
+import Btn from "@/elements/buttons/Btn";
+import { useState } from "react";
+import { RiDeleteBinLine, RiEditBoxLine } from "react-icons/ri";
+import { Col, Row } from "reactstrap";
+import AddressTable from "./AddressTable";
+import request from "@/utils/axiosUtils";
+import AccountContext from "@/helper/accountContext";
 
-const AddressData = ({ addressState, setAddressState, modal, setModal, setEditAddress }) => {
-  const [deleteId, setDeleteId] = useState('');
+const AddressData = ({
+  addressState,
+  setAddressState,
+  modal,
+  setModal,
+  setEditAddress,
+}) => {
+  const [deleteId, setDeleteId] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+  const { accountData, setAccountData } = useContext(AccountContext);
+
   const removeAddress = async () => {
-    // Add remove address login here
-    setAddressState((prev) => prev.filter((elem) => elem.id !== deleteId));
-    setModal('');
+    setIsDeleting(true);
+    try {
+      // Call API to delete address
+      await request({
+        method: "DELETE",
+        url: `/address`,
+        data: { id: deleteId },
+      });
 
-    await request({
-      method: 'DELETE',
-      url: `/address`,
-      data: { id: deleteId },
-    });
+      // Only after successful API call, remove from local state
+      setAddressState((prev) => prev.filter((elem) => elem.id !== deleteId));
 
+      // Update AccountContext to remove the deleted address
+      if (accountData?.address) {
+        setAccountData((prev) => ({
+          ...prev,
+          address: prev.address.filter((elem) => elem.id !== deleteId),
+        }));
+      }
+
+      setModal("");
+    } catch (error) {
+      console.error("Failed to delete address:", error);
+      // Optionally show error message to user
+      alert("Failed to delete address. Please try again.");
+    } finally {
+      setIsDeleting(false);
+    }
   };
+
   return (
-    <Row className='g-sm-4 g-3'>
+    <Row className="g-sm-4 g-3">
       {addressState?.map((address, i) => (
         <Col xxl={4} xl={6} lg={12} md={6} key={i}>
-          <div className='address-box'>
+          <div className="address-box">
             <AddressTable address={address} />
-            <div className='button-group'>
+            <div className="button-group">
               <Btn
-                className='btn-sm add-button'
-                title={'Edit'}
+                className="btn-sm add-button"
+                title={"Edit"}
                 onClick={() => {
                   setEditAddress(address);
-                  setModal('edit');
-                }}>
+                  setModal("edit");
+                }}
+              >
                 <RiEditBoxLine />
               </Btn>
               <Btn
-                className='btn-sm add-button'
-                title={'Remove'}
+                className="btn-sm add-button"
+                title={"Remove"}
                 onClick={() => {
                   setDeleteId(address?.id);
-                  setModal('remove');
-                }}>
+                  setModal("remove");
+                }}
+              >
                 <RiDeleteBinLine />
               </Btn>
             </div>
           </div>
         </Col>
       ))}
-      <ConfirmDeleteModal modal={modal == 'remove'} setModal={setModal} confirmFunction={removeAddress} setDeleteId={setDeleteId} />
+      <ConfirmDeleteModal
+        modal={modal == "remove"}
+        setModal={setModal}
+        confirmFunction={removeAddress}
+        setDeleteId={setDeleteId}
+        loading={isDeleting}
+      />
     </Row>
   );
 };
--- src/components/common/ConfirmDeleteModal.jsx
+++ src/components/common/ConfirmDeleteModal.jsx
@@ -1,21 +1,45 @@
-import Btn from '@/elements/buttons/Btn';
-import { RiDeleteBinLine } from 'react-icons/ri';
-import CustomModal from './CustomModal';
+import Btn from "@/elements/buttons/Btn";
+import { RiDeleteBinLine } from "react-icons/ri";
+import CustomModal from "./CustomModal";
 
-const ConfirmDeleteModal = ({ modal, setModal,  confirmFunction, setDeleteId }) => {
+const ConfirmDeleteModal = ({
+  modal,
+  setModal,
+  confirmFunction,
+  setDeleteId,
+  loading = false
+}) => {
   return (
     <>
-      <CustomModal modal={modal} setModal={setModal} classes={{ modalClass: 'theme-modal delete-modal', modalHeaderClass: 'p-0' }}>
-        <RiDeleteBinLine className='icon-box' />
-        <h5 className='modal-title'>{'Delete Item ?'}</h5>
-        <p>{"This Item Will Be Deleted Permanently. You Can't Undo This Action."} </p>
-        <div className='button-box'>
-          <Btn title='No' className='btn btn-md btn-theme-outline fw-bold' onClick={() => {
-            setDeleteId && setDeleteId();
-            setModal('');
-          }}
+      <CustomModal
+        modal={modal}
+        setModal={setModal}
+        classes={{
+          modalClass: "theme-modal delete-modal",
+          modalHeaderClass: "p-0",
+        }}
+      >
+        <RiDeleteBinLine className="icon-box" />
+        <h5 className="modal-title">{"Delete Item ?"}</h5>
+        <p>
+          {"This Item Will Be Deleted Permanently. You Can't Undo This Action."}{" "}
+        </p>
+        <div className="button-box">
+          <Btn
+            title="No"
+            disabled={loading}
+            className="btn btn-md btn-theme-outline fw-bold"
+            onClick={() => {
+              setDeleteId && setDeleteId();
+              setModal("");
+            }}
           />
-          <Btn title='Yes' className='theme-bg-color btn-md fw-bold text-light' onClick={confirmFunction} />
+          <Btn
+            title={loading ? "Deleting..." : "Yes"}
+            className="theme-bg-color btn-md fw-bold text-light"
+            onClick={confirmFunction}
+            disabled={loading}
+          />
         </div>
       </CustomModal>
     </>
